2ab9bab8abf9934ef887e120194af40e4d65fada
sahib <sahibhayatsbht44@gmail.com>
2024-01-12 16:29:54 +0500

    connect to deb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGDB_URI)
+.then(() => {
+    console.log('Connected to mongoDB')
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const app = express();
+
 
 app.listen(3000, () => {
     console.log("Server is running on port no! 3000 ")
