23b8d32f1a309b2e84798001bf62fd3a445c6beb
sahib <sahibhayatsbht44@gmail.com>
2024-01-12 16:04:46 +0500

    added headers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
