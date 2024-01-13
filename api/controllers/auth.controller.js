6fdc493666f55a831b35a429f5d66928b7940f35
sahib <sahibhayatsbht44@gmail.com>
2024-01-13 09:49:46 +0500

    added middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,22 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
-        } else {
-
-            const hashedPassword =  bcryptjs.hashSync(password, 10);
-
-            const newUser = new User({ username, email, password: hashedPassword });
-            await newUser.save();
-            res.status(201).json({ message: "User created successfully" })
         }
+
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully" })
+
     } catch (error) {
-        res.status(500).json(error.message);
+        next(errorHandler(300, "Somthing went wrong"));
     }
 }
