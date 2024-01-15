1ef222eeaa2010f224a4d78783d28b3db666c47c
sahib <sahibhayatsbht44@gmail.com>
2024-01-15 21:03:47 +0500

    complete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto '>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About</h1>
+      <p className='mb-4 text-slate-700 '>This is a full-stack web application built with the 
+        MERN (mongo, Express, React, Node.js) stack, import PropTypes from 'prop-types'
+        includes authentication features that allow users to 
+        sign up . log in and log out and provides acces to
+        protected routes only for authentication users.</p>
+      <p className='mb-4 text-slate-700 '>This is a full-stack web application built with the 
+        MERN (mongo, Express, React, Node.js) stack, import PropTypes from 'prop-types'
+        includes authentication features that allow users to 
+        sign up . log in and log out and provides acces to
+        protected routes only for authentication users.</p>
+      <p className='mb-4 text-slate-700 '>This is a full-stack web application built with the 
+        MERN (mongo, Express, React, Node.js) stack, import PropTypes from 'prop-types'
+        includes authentication features that allow users to 
+        sign up . log in and log out and provides acces to
+        protected routes only for authentication users.</p>
+    </div>
   )
 }
 
